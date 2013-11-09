4e3353a3ec0ebd1e92fd574bad822fe75abd0089
Shageevan Sachithanandan <geevz21@gmail.com>
2013-11-09 14:22:11 +0530

    Bootstrap and Font Awesome added. Basic autoload configuration. Common header and footer.

---
+++ app/application/views/common/footer.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+    <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ app/application/views/common/header.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CodeIgniter Application Starter Kit</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    <script src="https://code.jquery.com/jquery.js"></script>
+  </head>
+  <body>
+    
